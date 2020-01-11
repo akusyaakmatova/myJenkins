474a67138b29cde72f4d1e4c93d0b5a0d4be83b4
Amir Smail <amirsmail@Amirs-MacBook-Pro-2.local>
2020-01-11 17:22:15 -0600

    first commit

---
+++ src/test/java/jenkinsIntroPackage/FirstTest.java
@@ -0,0 +1,11 @@
+package jenkinsIntroPackage;
+
+import org.junit.Test;
+
+public class FirstTest {
+    @Test
+    public void secondTest(){
+        System.out.println("test from first class");
+
+    }
+}
